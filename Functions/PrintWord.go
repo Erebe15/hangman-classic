58dfa78d216598fc4ed7632e8878d2c00f20bca1
Rodolphe Andrieux <rodolpheabike@gmail.com>
2022-10-06 22:46:55 +0200

    refactor(#12): add print word and visual indication
    added some "prints" to see clearly what each function does in a debug purpose

--- Functions/PrintWord.go
+++ Functions/PrintWord.go
@@ -1,3 +1,15 @@
 package hangman
 
-//func PrintWord()
+import "fmt"
+
+func PrintWord(Word string, LetterList []string) {
+	fmt.Printf("liste de lettre revealed: %s\n", LetterList)
+	for i := 0; i < len(Word); i++ {
+		if DoesContain(LetterList, string(Word[i])) == true {
+			fmt.Print(string(Word[i]))
+		} else {
+			fmt.Print("_")
+		}
+	}
+	fmt.Print("\n")
+}
--- StartAGame.go
+++ StartAGame.go
@@ -1,9 +1,16 @@
 package main
 
+import hangman "hangman/Functions"
+
 func StartPlaying(GameInProgress Game) {
-	for GameInProgress.Tries <= 10 && !WordCompleted(GameInProgress) {
+	print("tries : ")
+	hangman.PrintWord(GameInProgress.Word, GameInProgress.RevealedLettres)
+	for GameInProgress.Tries < 10 && !WordCompleted(GameInProgress) {
 		// print word
 		//ask for input and ad it to the list of guess
+		print(GameInProgress.Tries, ", ")
 		GameInProgress.Tries++
 	}
+	println(GameInProgress.Tries)
+	println("ggwp...")
 }
