2f9161345c242e890293cb9c3d32549dce38f1db
thomas <thomas.piet@ynov . com>
2022-10-07 16:10:21 +0200

    feat(#3): manage ChooseLetter.go
    add  letter verification

--- ChooseLetter.go
+++ ChooseLetter.go
@@ -4,9 +4,31 @@ import (
 	"fmt"
 )
 
-func ChooseLetter(GameInProgress Game) string {
+func ChooseLetter(GameInProgress Game) Game {
 	var letter string
 	fmt.Scanln(&letter)
-	println("letter: ", letter)
-	return letter
+	if !IsAlpha(letter) {
+		fmt.Println("Please, select a letter, try again. ")
+		fmt.Println("")
+		fmt.Println("___________________________________")
+	} else if len(letter) > 1 {
+		fmt.Println("Please, select only one letter, try again. ")
+		fmt.Println("")
+		fmt.Println("___________________________________")
+	} else {
+		println("letter: ", letter)
+	}
+	return GameInProgress
+}
+
+func IsAlpha(s string) bool {
+	for i := 0; i < len(s); i++ {
+		if s[i] > 96 && s[i] < 123 {
+			return true
+		}
+		if s[i] > 64 && s[i] < 91 {
+			return true
+		}
+	}
+	return false
 }
