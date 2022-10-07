0223f6ae9269523b92a827d0b6c2e65f007d290d
Rodolphe Andrieux <rodolpheabike@gmail.com>
2022-10-07 15:24:39 +0200

    fix(#3): new letter in uppercase
    upper case

--- ChooseLetter.go
+++ ChooseLetter.go
@@ -2,21 +2,13 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func ChooseLetter(GameInProgress Game) Game {
 	var letter string
-	fmt.Scan(&letter)
+	fmt.Scanln(&letter)
 	println("letter: ", letter)
-	GameInProgress.RevealedLettres = append(GameInProgress.RevealedLettres, letter)
+	GameInProgress.RevealedLettres = append(GameInProgress.RevealedLettres, strings.ToUpper(letter))
 	return GameInProgress
-	/*for i := 0; i < len(Word)-1; i++ {
-		if DoesContain(starter, Word) == true {
-			fmt.Print(string(Word[i]))
-		} else {
-			fmt.Print("_")
-
-		}
-	}
-	*/
 }
