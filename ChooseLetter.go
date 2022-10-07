1026b94f6b528aeeebac5ab848ce44f2d72efb16
thomas <thomas.piet@ynov . com>
2022-10-07 20:03:19 +0200

    feat(#3): ChooseLetter.go
    add feat for already choosen letter (false)

--- ChooseLetter.go
+++ ChooseLetter.go
@@ -7,8 +7,8 @@ import (
 )
 
 func ChooseLetter(GameInProgress Game) string {
-	//fmt.Printf("Choose a letter : ")
 	var letter string
+	//var letterFalse []string
 	letterValid := false
 	for !letterValid {
 		fmt.Printf("Choose a letter : ")
@@ -18,19 +18,22 @@ func ChooseLetter(GameInProgress Game) string {
 			fmt.Println("Please, select a letter, try again. ")
 			fmt.Println("")
 			fmt.Println("___________________________________")
-
 		} else if len(letter) > 1 {
 			fmt.Println("Please, select only one letter, try again. ")
 			fmt.Println("")
 			fmt.Println("___________________________________")
-			letterValid = false
 		} else if hangman.DoesContain(GameInProgress.RevealedLettres, letter) {
+			fmt.Println("This letter has already been chosen")
+			fmt.Println("")
+			fmt.Println("___________________________________")
+		} else if hangman.DoesContain(GameInProgress.guess, letter) {
 			fmt.Println("This letter has already been chosen")
 			fmt.Println("")
 			fmt.Println("___________________________________")
 		} else {
 			letterValid = true
 			println("letter: ", letter)
+			GameInProgress.guess = append(GameInProgress.guess, letter)
 		}
 	}
 	return letter
--- StartAGame.go
+++ StartAGame.go
@@ -7,7 +7,6 @@ import (
 
 func StartPlaying(GameInProgress Game) { // go file in root to use struc "game" freely
 	hangman.PrintWord(GameInProgress.Word, GameInProgress.RevealedLettres)
-	//fmt.Printf("Choose a letter : ")
 	for GameInProgress.Tries < 10 && !WordCompleted(GameInProgress) {
 		guess := ChooseLetter(GameInProgress)
 		GameInProgress.RevealedLettres = append(GameInProgress.RevealedLettres, strings.ToUpper(guess))
