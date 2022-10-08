e4b086c1eb71d7a9438d351afcfa983c3f2b4075
Rodolphe Andrieux <rodolpheabike@gmail.com>
2022-10-08 23:36:21 +0200

    feat(#17): use split
    use split func instead of a long code

--- Functions/GetJose.go
+++ Functions/GetJose.go
@@ -3,7 +3,7 @@ package hangman
 import (
 	"log"
 	"os"
-	"runtime"
+	"strings"
 )
 
 func GetJose() []string {
@@ -11,21 +11,7 @@ func GetJose() []string {
 	if err != nil {
 		log.Fatal(err)
 	}
-	os := runtime.GOOS //runtime.GOOS -> linux, windows, darwin etc.
-
 	var JosePositions []string
-	var char string
-	for i, l := range data {
-		if string(l) == "\n" && string(data[i-1]) == "\n" {
-			if os == "windows" {
-				JosePositions = append(JosePositions, char[:len(char)-2]) // -1 getting rid of the backslash (byte '13')
-			} else {
-				JosePositions = append(JosePositions, char[:len(char)])
-			}
-			char = ""
-		} else {
-			char = char + string(l)
-		}
-	}
+	JosePositions = strings.Split(string(data), "♦")
 	return JosePositions
 }
