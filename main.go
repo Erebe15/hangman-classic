52491a60550dbe8b2941679aefb2595d9c81458d
Rodolphe Andrieux <rodolpheabike@gmail.com>
2022-10-10 14:08:11 +0200

    feat(#19): welcome
    changed the welcome message position

--- main.go
+++ main.go
@@ -16,19 +16,19 @@ func main() {
 	var GameInProgress Game
 	PlayAgain := true
 	choice := ""
+	hangman.PrintRules()
 	for PlayAgain {
 		GameInProgress.Word = hangman.GetWord()
 		GameInProgress.RevealedLettres = hangman.RevealStartLettres(GameInProgress.Word)
 		GameInProgress.Tries = 0
 		GameInProgress.JoseStates = hangman.GetJose()
-		hangman.PrintRules()
 		StartPlaying(GameInProgress)
 
 		fmt.Println("")
 		fmt.Println("Do you want to play again ?")
 		fmt.Printf("Enter 'y' to play again, or any other input to quit : ")
 		fmt.Scanln(&choice)
-		if strings.ToUpper(choice) == "Y" {
+		if strings.ToUpper(choice) == "Y" || strings.ToUpper(choice) == "YES" {
 			PlayAgain = true
 		} else {
 			fmt.Println("See you later!")
