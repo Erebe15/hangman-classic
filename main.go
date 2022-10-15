f7394ab6f4083010e0a17eb6a66aa87be67526bd
Rodolphe Andrieux <rodolpheabike@gmail.com>
2022-10-16 00:41:52 +0200

    fix(#19): main update
    main was buggy

--- main.go
+++ main.go
@@ -3,51 +3,61 @@ package main
 import (
 	"fmt"
 	"hangman/Functions"
-	"os"
-	"os/exec"
 	"strings"
+	"time"
 )
 
 type Game struct {
-	Word                                     string
-	Tries                                    int
-	guess, RevealedLettres, JoseStates, save []string
+	Word                               string
+	Tries                              int
+	guess, RevealedLettres, JoseStates []string
 }
 
-func (Game) init() Game {
-	var GameInProgress Game
-	GameInProgress.Word = hangman.GetWord()
-	GameInProgress.RevealedLettres = hangman.RevealStartLettres(GameInProgress.Word)
+type Window struct {
+	ligns, colones int
+}
+
+var GameInProgress Game
+var w Window
+
+func Init() (bool, string) {
+	w.ligns, w.colones = hangman.Size()
 	GameInProgress.Tries = 0
 	GameInProgress.JoseStates = hangman.GetJose()
-	return GameInProgress
+	return true, ""
+}
+
+func welcome(l chan int) {
+	hangman.Clear()
+	time.Sleep(time.Second * 1)
+	PrintAscii(3, w.colones/2-(len("<WELCOME>")*12)/2, "\x1B[7mWELCOME")
+	PrintAscii(12, w.colones/2-(len("<WELCOME>")*12)/2, "  TO   ")
+	PrintAscii(21, w.colones/2-(len("<WELCOME>")*12)/2, "HANGMAN\x1B[0m")
+	time.Sleep(time.Second * 2)
+	hangman.Clear()
+	l <- 1
 }
 
 func main() {
-	c := exec.Command("clear")
-	c.Stdout = os.Stdout
-	c.Run()
-	var GameInProgress Game
-	PlayAgain := true
-	choice := ""
+	loading := make(chan int)
+	go welcome(loading)
+	PlayAgain, choice := Init()
 
-	if os.Args[1] == "--startwith" {
-		GameInProgress.readJSON(os.Args[2] + ".json")
-
-	} else {
-		GameInProgress = GameInProgress.init()
-	}
+	_ = <-loading
 	for PlayAgain {
 
-		hangman.PrintRules()
-		StartPlaying(GameInProgress)
+		CreateWindows()
+		go resizeWindow()
+		GameInProgress.Word = hangman.GetWord()
+		fmt.Println(MoveTo(3, 2), "word:", GameInProgress.Word)
+		GameInProgress.RevealedLettres = hangman.RevealStartLettres(GameInProgress.Word)
+		StartPlaying()
 
 		fmt.Println("")
 		fmt.Println("Do you want to play again ?")
 		fmt.Printf("Enter 'y' to play again, or any other input to quit : ")
 		fmt.Scanln(&choice)
-		if strings.ToUpper(choice) == "Y" {
-			GameInProgress = GameInProgress.init()
+		if strings.ToUpper(choice) == "Y" || strings.ToUpper(choice) == "YES" {
 			PlayAgain = true
 		} else {
 			fmt.Println("See you later!")
